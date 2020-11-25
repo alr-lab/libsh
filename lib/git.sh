870ad30f82847de30db28494cec4e66e79f30462
Alexandre Le Roy <ale-roy@student.42.fr>
2020-11-25 19:15:12 +0100

    Update license

--- lib/git.sh
+++ lib/git.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 #
-#######################################
 # Git Bash library
-#######################################
-# Personal Bash library
-# Copyright (C) 2020 Alexandre Le Roy
-# https://github.com/alr-lab/libsh
-#######################################
-# This program is free software: you can redistribute it
-# and/or modify it under the terms of the GNU General Public
-# License as published by the Free Software Foundation,
-# either version 3 of the License, or (at your option) any
-# later version.
-# 
-# This program is distributed in the hope that it will be
-# useful, but WITHOUT ANY WARRANTY; without even the implied
-# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-# PURPOSE. See the GNU General Public License for more
-# details.
-# 
-# You should have received a copy of the GNU General Public
-# License along with this program. If not, see
-# <https://www.gnu.org/licenses/>.
-#######################################
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# (c) 2020, Alexandre Le Roy <ale-roy@student.42.fr>
 
 #######################################
 # Prints the current Git branch
